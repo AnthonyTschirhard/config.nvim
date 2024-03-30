071133f6068f066a9dd06da2386f3e6d2b45f958
Anthony Tschirhard <anthony.tschirhard@brother.ca>
2024-03-30 15:29:54 -0400

    add ability to search for document symbols

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>sf', function()
   builtin.find_files { hidden = true }
 end, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>so', builtin.lsp_document_symbols, { desc = '[S]earch Symb[O]ls in Document' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
