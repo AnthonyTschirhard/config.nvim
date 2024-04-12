bfac9dd3b134e55df015f66d1fee3b76828db375
Anthony Tschirhard <anthony.tschirhard@brother.ca>
2024-04-12 11:47:58 -0400

    added completion with lsp-signature

--- lua/kickstart/lazy.lua
+++ lua/kickstart/lazy.lua
@@ -291,4 +291,10 @@ require('lazy').setup {
       'kristijanhusak/vim-dadbod-completion',
     },
   },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts) require 'lsp_signature'.setup(opts) end
+  }
 }
