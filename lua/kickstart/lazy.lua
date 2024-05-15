93bb2c1f6e557373d8fe1b81ea4bfc86f1ac143d
Anthony TSCHIRHARD <anthony.tschirhard@brother.ca>
2024-05-15 18:38:09 +0000

    added tmux config

--- lua/kickstart/lazy.lua
+++ lua/kickstart/lazy.lua
@@ -296,5 +296,27 @@ require('lazy').setup {
     event = "VeryLazy",
     opts = {},
     config = function(_, opts) require 'lsp_signature'.setup(opts) end
+  },
+
+  -- schema store
+  "b0o/schemastore.nvim",
+
+  -- TMUX
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   }
 }
