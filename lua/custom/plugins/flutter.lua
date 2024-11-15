ec4ae7e447440c5a05266c0d81fc1c920a12f2d4
Anthony Tschirhard <anthony.tschirhard@gmail.com>
2024-11-15 17:14:51 -0500

    added flutter tools

---
+++ lua/custom/plugins/flutter.lua
@@ -0,0 +1,27 @@
+-- Build flutter and dart applications in neovim using the native LSP
+-- https://github.com/nvim-flutter/flutter-tools.nvim
+return {
+	'nvim-flutter/flutter-tools.nvim',
+	lazy = false,
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'stevearc/dressing.nvim', -- optional for vim.ui.select
+	},
+	config = function()
+		--
+		-- alternatively you can override the default configs
+		require("flutter-tools").setup({})
+		vim.keymap.set("n", "<leader>flr", ":FlutterRun<CR>", { desc = "[Fl]utter [R]un" })
+		vim.keymap.set("n", "<leader>fle", ":FlutterEmulators<CR>", { desc = "[Fl]utter [E]mulators" })
+		vim.keymap.set("n", "<leader>fld", ":FlutterDevices<CR>", { desc = "[Fl]utter [D]evices" })
+		-- vim.keymap.set("n", "<leader>flo", ":FlutterReload<CR>", { desc = "[Fl]utter [R]eload" })
+		vim.keymap.set("n", "<leader>fls", ":FlutterRestart<CR>", { desc = "[Fl]utter re[S]tart" })
+
+		require("telescope").load_extension("flutter")
+		-- vim.keymap.set(
+		-- 	"n",
+		-- 	"<leader>su",
+		-- 	require("telescope").extensions.flutter.commands
+		-- 	{ desc = "[S]earch Fl[u]tter" })
+	end,
+}
