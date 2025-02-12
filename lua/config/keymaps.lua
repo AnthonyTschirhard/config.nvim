8079fb8b390a79f607ca308ab5b0a8f912f7c231
Anthony Tschirhard <anthony.tschirhard@gmail.com>
2025-02-12 14:22:39 -0500

    added keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Navigation
+vim.keymap.set({ "n", "v" }, "<A-j>", "b", { desc = "Backward one word" })
+vim.keymap.set({ "n", "v" }, "<A-k>", "w", { desc = "Forward one word" })
+
+-- split windows
+vim.keymap.set("n", "<leader>vs", ":vs<CR>")
+vim.keymap.set("n", "<leader>hs", ":split<CR>")
