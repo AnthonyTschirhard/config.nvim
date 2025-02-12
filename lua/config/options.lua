851ce5146fa468a7628c2f436157e04452e25785
Anthony Tschirhard <anthony.tschirhard@gmail.com>
2025-02-12 14:22:42 -0500

    added options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Rulers
+vim.opt.colorcolumn = "72,80"
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
