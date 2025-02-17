42051801464b900741161a986d672d612da12c13
Anthony Tschirhard <anthony.tschirhard@gmail.com>
2025-02-17 13:04:16 -0500

    added ciw keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,7 @@
 vim.keymap.set({ "n", "v" }, "<A-j>", "b", { desc = "Backward one word" })
 vim.keymap.set({ "n", "v" }, "<A-k>", "w", { desc = "Forward one word" })
 
--- split windows
-vim.keymap.set("n", "<leader>vs", ":vs<CR>")
-vim.keymap.set("n", "<leader>hs", ":split<CR>")
+-- text edition
+vim.keymap.set("n", "yY", ":%y+<CR>")
+vim.keymap.set("n", "<A-BS>", "ciw")
+vim.keymap.set("i", "<A-BS>", "<C-w>")
