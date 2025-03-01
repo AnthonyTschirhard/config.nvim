9a5e4f7a7949efa484358800a5ee9b48cb4f9422
Anthony Tschirhard <anthony.tschirhard@gmail.com>
2025-02-28 19:12:03 -0500

    flutter tools configured

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -11,13 +11,30 @@ return {
     --
     -- alternatively you can override the default configs
 
-    require("flutter-tools").setup({})
+    require("flutter-tools").setup({
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+        exception_breakpoints = {},
+        register_configurations = function(paths)
+          local dap = require("dap")
+          -- See also: https://github.com/akinsho/flutter-tools.nvim/pull/292
+          dap.adapters.dart = {
+            type = "executable",
+            command = paths.flutter_bin,
+            args = { "debug-adapter" },
+          }
+          dap.configurations.dart = {}
+          require("dap.ext.vscode").load_launchjs()
+        end,
+      },
+    })
+
     require("telescope").load_extension("flutter")
 
     vim.keymap.set("n", "<leader>flr", ":FlutterRun --no-enable-impeller<CR>", { desc = "[Fl]utter [R]un" })
     vim.keymap.set("n", "<leader>fle", ":FlutterEmulators<CR>", { desc = "[Fl]utter [E]mulators" })
     vim.keymap.set("n", "<leader>fld", ":FlutterDevices<CR>", { desc = "[Fl]utter [D]evices" })
-    -- vim.keymap.set("n", "<leader>flo", ":FlutterReload<CR>", { desc = "[Fl]utter [R]eload" })
     vim.keymap.set("n", "<leader>fls", ":FlutterRestart<CR>", { desc = "[Fl]utter re[S]tart" })
     vim.keymap.set("n", "<leader>flc", ":Telescope flutter commands<CR>", { desc = "[Fl]utter [C]ommands" })
   end,
