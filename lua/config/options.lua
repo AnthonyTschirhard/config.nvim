21e2b9aec5c6e5aa4da4ddbd1ffed9cdd28c5131
Anthony Tschirhard <anthony.tschirhard@gmail.com>
2025-04-30 17:01:02 -0400

    pyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+
 -- Save undo history
 vim.opt.undofile = true
 
